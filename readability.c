d3f6942e6d13c949408facd0506760dfab94d2c2
pravinkumarsp <83107692+pravinkumarsp@users.noreply.github.com>
2021-10-09 18:35:47 +0530

    Add files via upload

---
+++ readability.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<cs50.h>
+#include<string.h>
+#include<math.h>
+
+int main(void)
+{
+    string text = get_string("text: ");
+    int letterscount = 0;
+    int wordcount = 1;
+    int sentencecount = 0;
+    
+    // count words, letters, sentence
+    
+    for (int i = 0; i < strlen(text); i++)
+    {
+        if ((text[i] >= 'a'&& text[i] <= 'z') || (text[i] >= 'A' && text[i] <= 'Z'))        
+        {
+            letterscount = letterscount + 1;
+        }
+        else if (text[i] == ' ')
+        {
+            wordcount = wordcount + 1;
+        }
+        else if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+        {
+            sentencecount = sentencecount + 1;
+        }
+    }
+    
+    // To find the grade level using coleman - liau index 
+    
+    float grade = 0.0588 * (100 * (float) letterscount / (float) wordcount) - 0.296 * (100 * (float) sentencecount / (float) wordcount) - 15.8;
+    if (grade < 16 && grade >= 0)
+    {
+        printf("Grade %i\n", (int) round(grade));
+    }
+    else if (grade >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Before Grade 1\n");
+    }
+
+}
+        
+        
+            
+
+    
